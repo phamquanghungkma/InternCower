3ba540e8e2d5939be9a1ff7eea2e7a0c4f223c18
Tofu <athenavladimir@gmail.com>
2020-12-08 12:24:09 +0700

    update

--- InternCower/Login/LoginVC.swift
+++ InternCower/Login/LoginVC.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class LoginVC: UIViewController {
     static let identifier = "LoginVc"
+    
+    @IBOutlet weak var EmailTF: UITextField!
+    
+    @IBOutlet weak var PassTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+       
+        setUpView()
+    }
+    func setUpView(){
+        self.EmailTF.placeholder = "Email"
+               EmailTF.layer.borderWidth = 1.0
+                  let myColor = UIColor.white
+               EmailTF.layer.borderColor = myColor.cgColor
+        
+        self.PassTF.placeholder = "Password"
+        PassTF.layer.borderWidth = 1.0
+        PassTF.layer.borderColor = myColor.cgColor
+        
     }
 
 
